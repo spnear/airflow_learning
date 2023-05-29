dcbf74422e11881a1814849b61c939f5bb98168f
Summan Digital Twin <dtw.argos@summan.com>
2023-05-29 13:36:42 -0500

    Finalizada ETL extracccion desde api de random user

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
@@ -25,15 +26,23 @@ def _process_user(ti):
     user = user['results'][0]
     processed_user = json_normalize({
         'firstname': user['name']['first'],
-        'firstname': user['name']['last'],
-        'firstname': user['location']['country'],
-        'firstname': user['login']['username'],
-        'firstname': user['login']['password'],
-        'firstname': user['email']
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email': user['email']
     })
-
+    print(processed_user)
     processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
 
+
+def _store_user():
+
+    hook = PostgresHook(postgres_conn_id='postgres')
+    hook.copy_expert(
+        sql="COPY users FROM stdin WITH DELIMITER as ','",
+        filename='/tmp/processed_user.csv'
+    )
 
 with DAG('user_processing', start_date=START_DATE,
          schedule_interval='@daily',
@@ -67,4 +76,11 @@ with DAG('user_processing', start_date=START_DATE,
         python_callable=_process_user
     )
 
-    extract_user >> process_user
+
+    store_user = PythonOperator(
+        task_id='store_user',
+        python_callable=_store_user
+    )
+
+    create_table >> is_api_available >> extract_user
+    extract_user >> process_user >> store_user
