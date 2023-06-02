64d3c502f8ca4295c3240d089d4d67cdb45f81a2
Summan Digital Twin <dtw.argos@summan.com>
2023-06-01 21:14:42 -0500

    Added subdag example to group common tasks and avoid repeating in the same .py file

---
+++ dags/group_dag.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+
+from subdags.subdag_downloads import subdag_downloads
+from subdags.subdag_transforms import subdag_transforms
+ 
+from datetime import datetime
+
+START_DATE = datetime(2023, 1, 1)
+ 
+with DAG('group_dag', start_date=START_DATE, 
+    schedule_interval='@daily', catchup=False) as dag:
+
+    args = {'start_date': dag.start_date, 'schedule_interval': dag.schedule_interval, 'catchup': dag.catchup}
+    
+    downloads = SubDagOperator(
+        task_id='downloads',
+        subdag=subdag_downloads(dag.dag_id, 'downloads', args)
+    )
+
+    check_files = BashOperator(
+        task_id='check_files',
+        bash_command='sleep 10'
+    )
+ 
+    transforms = SubDagOperator(
+        task_id='transforms',
+        subdag=subdag_transforms(dag.dag_id, 'transforms', args)
+    )
+    downloads >> check_files >> transforms
